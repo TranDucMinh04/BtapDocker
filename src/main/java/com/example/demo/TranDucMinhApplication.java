e5c8111deecbfe696d281b095c03ccb6ef85b1f2
TranDucMinh1502 <tranducminh15022004@gmail.com>
2026-03-19 10:19:52 +0700

    feat: dockerfile

---
+++ src/main/java/com/example/demo/TranDucMinhApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranDucMinhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranDucMinhApplication.class, args);
+	}
+
+}
